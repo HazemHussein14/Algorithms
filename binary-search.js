1c21efd320561e9388c7f96e602f0242cb70e22e
Hazem Hussein <113997542+HazemHussein14@users.noreply.github.com>
2024-02-04 01:20:17 +0200

    Added binary-search algo

---
+++ binary-search.js
@@ -0,0 +1,21 @@
+function binarySearch(arr, key) {
+  let low = 0;
+  let high = arr.length - 1;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (key === arr[mid]) {
+      return mid;
+    } else if (key > arr[mid]) {
+      low = mid + 1;
+    } else if (key < arr[mid]) {
+      high = mid - 1;
+    } else {
+      return -1;
+    }
+  }
+}
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+
+console.log(binarySearch(arr, 3));
