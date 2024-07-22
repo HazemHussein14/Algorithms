6d258295af38caa4c2e365a1e5091e84ddabbece
Hazem Hussein <113997542+HazemHussein14@users.noreply.github.com>
2024-07-22 08:49:22 +0300

    implement breadth first search algorithm

---
+++ graph-traversal-algorithms.js
@@ -0,0 +1,81 @@
+import Queue from "./Data-Structures/Queue.js";
+
+class Vertex {
+  constructor() {
+    this.name = "";
+    this.visited = false;
+    this.vertexLinks = [];
+  }
+}
+
+class Edge {
+  constructor(source, target, weight = 0) {
+    this.source = source;
+    this.target = target;
+    this.weight = weight;
+  }
+}
+
+class Graph {
+  constructor(names) {
+    this.lastIndex = 0;
+    this.vertices = [];
+    for (const name of names) {
+      this.vertices[this.lastIndex] = new Vertex();
+      this.vertices[this.lastIndex].name = name;
+      this.lastIndex++;
+    }
+  }
+
+  addEdges(vertexIndex, targets) {
+    for (let i = 0; i < targets.length; i++) {
+      this.vertices[vertexIndex].vertexLinks[i] = new Edge(
+        this.vertices[vertexIndex],
+        this.vertices[targets[i]]
+      );
+    }
+  }
+
+  BFS() {
+    console.log("BFS running");
+    const q = new Queue();
+
+    q.enqueue(this.vertices[0]);
+    this.vertices[0].visited = true;
+
+    let currentVertex;
+    let destinations = [];
+
+    while (q.hasData()) {
+      currentVertex = q.dequeue();
+      destinations = currentVertex.vertexLinks;
+
+      for (let i = 0; i < destinations.length; i++) {
+        if (destinations[i].target.visited == false) {
+          q.enqueue(destinations[i].target);
+          destinations[i].target.visited = true;
+          console.log(currentVertex.name + " - " + destinations[i].target.name);
+        }
+      }
+    }
+    this.restoreVertices();
+  }
+
+  restoreVertices() {
+    for (const v of this.vertices) {
+      v.visited = false;
+    }
+  }
+}
+
+const graph = new Graph(["A", "B", "C", "D", "E", "F", "G", "H", "I"]);
+graph.addEdges(0, [1, 2]);
+graph.addEdges(1, [0, 3, 4]);
+graph.addEdges(2, [0, 3, 5]);
+graph.addEdges(3, [1, 2, 4]);
+graph.addEdges(4, [1, 5]);
+graph.addEdges(5, [2, 3, 4, 7]);
+graph.addEdges(6, [7, 8]);
+graph.addEdges(7, [5, 6, 8]);
+graph.addEdges(8, [6, 7]);
+graph.BFS();
